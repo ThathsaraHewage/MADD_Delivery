6d49cbfcc508ff1e39983af9e0f6b0809700188b
Thathsara Hewage <67362351+ThathsaraHewage@users.noreply.github.com>
2020-09-19 22:48:36 +0530

    MADD mini project first two pages

--- app/src/main/java/com/example/usingstrings/ActivityTwo.java
+++ app/src/main/java/com/example/usingstrings/ActivityTwo.java
@@ -1,21 +1,14 @@
 package com.example.usingstrings;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.w3c.dom.Text;
+import android.os.Bundle;
 
-public class ActivityOne extends AppCompatActivity {
-
-        Text txtView2;
+public class ActivityTwo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_one);
-
-
+        setContentView(R.layout.activity_two);
     }
-
 }
